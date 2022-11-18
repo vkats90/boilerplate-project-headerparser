7c1bdb698ef6577afb8f4ab0ab6c1dae0cd20bda
vkats90 <mars77@gmail.com>
2022-11-17 21:52:00 -0700

    commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+app.set('trust proxy', true)
+const geoip=require('geoip-lite');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -22,6 +24,10 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami',function(req, res) {
+  res.json({"ipaddress":req.ip,"language":req.headers["accept-language"],"software":req.headers["user-agent"],});
 });
 
 // listen for requests :)
